b185111782d4a5f1e835060377022a7355bd5eec
Mobulan <mobulan@qq.com>
2024-03-28 18:11:58 +0800

    correct the lr for the pytorch amp

--- setup.py
+++ setup.py
@@ -8,11 +8,11 @@ config = SetupConfig(config, cfg_file)
 config.defrost()
 
 ## Log Name and Perferences
-config.write = True			# comment it to disable all the log writing
-config.train.checkpoint = True		# comment it to disable saving the checkpoint
+# config.write = True			# comment it to disable all the log writing
+# config.train.checkpoint = True		# comment it to disable saving the checkpoint
 config.misc.exp_name = f'{config.data.dataset}'
 config.misc.log_name = f'IELT'
-config.cuda_visible = '0,1,2,3'
+config.cuda_visible = '1'
 
 # Environment Settings
 config.data.log_path = os.path.join(config.misc.output, config.misc.exp_name, config.misc.log_name
@@ -26,7 +26,7 @@ os.environ['OMP_NUM_THREADS'] = '1'
 # Setup Functions
 config.nprocess, config.local_rank = SetupDevice()
 config.data.data_root, config.data.batch_size = LocateDatasets(config)
-config.train.lr = ScaleLr(config)
+# config.train.lr = ScaleLr(config)
 log = SetupLogs(config, config.local_rank)
 if config.write and config.local_rank in [-1, 0]:
 	with open(config.data.log_path + '/config.json', "w") as f:
